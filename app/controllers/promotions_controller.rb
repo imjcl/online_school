2e7b63c8d3e1712dd1b92a0a54bb15e91a4d3067
James Liu <jaycliu@gmail.com>
2014-07-24 20:19:27 -0700

    connect to endpoint for individual promotion

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -4,5 +4,8 @@ class PromotionsController < ApplicationController
   end
 
   def show
+    @promotion = JSON.parse RestClient.get "http://www.webappcamp.ws/api/v0/promotions/#{params[:id]}"
+
+    @promotion = @promotion["data"]
   end
 end
