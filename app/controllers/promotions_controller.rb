e8af655cda1673c547d3e5ff060b9188755c59d8
James Liu <jaycliu@gmail.com>
2014-07-24 20:14:46 -0700

    add show action for promotions controller

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -2,4 +2,7 @@ class PromotionsController < ApplicationController
   def index
     @promotions = JSON.parse RestClient.get 'http://www.webappcamp.ws/api/v0/promotions'
   end
+
+  def show
+  end
 end
