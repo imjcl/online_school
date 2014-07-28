dfd99e7623176486d2c33579098a34dcb5aee63c
James Liu <jaycliu@gmail.com>
2014-07-27 17:50:32 -0700

    add discount helper method

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -3,4 +3,10 @@ module PromotionsHelper
     currency = price.to_s.split ''
     currency = '$' + currency.insert(currency.length - 2, '.').join
   end
+
+  def discount promotion
+    amt = (promotion['original_price'].to_f - promotion['price']) / promotion['original_price']
+
+    (amt * 100).round(0).to_s + '%'
+  end
 end
