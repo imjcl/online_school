b5c07d9bc18dd021423d0f498ba15a43f194267d
James Liu <jaycliu@gmail.com>
2014-08-03 16:08:15 -0700

    add env variable to promotions controller

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,11 +1,11 @@
 class PromotionsController < ApplicationController
   
   def index
-    @promotions = JSON.parse RestClient.get 'http://sandbox.webappcamp.ws/api/v0/promotions'
+    @promotions = JSON.parse RestClient.get ENV['PROMO_ENDPOINT']
   end
 
   def show
-    @promotion = JSON.parse RestClient.get "http://sandbox.webappcamp.ws/api/v0/promotions/#{params[:id]}"
+    @promotion = JSON.parse RestClient.get "#{ENV['PROMO_ENDPOINT']}/#{params[:id]}"
 
     @promotion = @promotion["data"]
   end
