06928175a42256fd835af508ae902688ed432c08
James Liu <jaycliu@gmail.com>
2014-08-03 17:27:34 -0700

    rename promotions methods

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,9 +1,9 @@
 class PromotionsController < ApplicationController
   def index
-    @promotions = WebappcampServices::get_all
+    @promotions = WebappcampServices::get_all_promotions
   end
 
   def show
-    @promotion = WebappcampServices::get_by_id params['id']
+    @promotion = WebappcampServices::get_by_promotion_id params['id']
   end
 end
--- app/services/webappcamp_services.rb
+++ app/services/webappcamp_services.rb
@@ -1,11 +1,11 @@
 class WebappcampServices
   PROMOS = ENV['PROMO_ENDPOINT']
   
-  def self.get_all
+  def self.get_all_promotions
     JSON.parse RestClient.get PROMOS
   end
 
-  def self.get_by_id id
+  def self.get_by_promotion_id id
     (JSON.parse RestClient.get "#{PROMOS}/#{id}")['data']
   end
 end
