3f197d64c0699e4221539f32d9363f583c244f10
James Liu <jaycliu@gmail.com>
2014-08-03 17:23:11 -0700

    change webappcamp to hold a constant for the endpoint url

--- app/services/webappcamp_services.rb
+++ app/services/webappcamp_services.rb
@@ -1,9 +1,11 @@
 class WebappcampServices
+  PROMOS = ENV['PROMO_ENDPOINT']
+  
   def self.get_all
-    JSON.parse RestClient.get ENV['PROMO_ENDPOINT']
+    JSON.parse RestClient.get PROMOS
   end
 
   def self.get_by_id id
-    (JSON.parse RestClient.get "#{ENV['PROMO_ENDPOINT']}/#{id}")['data']
+    (JSON.parse RestClient.get "#{PROMOS}/#{id}")['data']
   end
 end
